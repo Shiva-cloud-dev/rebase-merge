30207ab2ee5e1f1e0d5d000279224e40ab517484
shiva <angatashiva916@gmail.com>
2025-06-01 08:32:16 +0530

    Developer-1 first change app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import boto3
+
+# Initialize S3 client
+s3 = boto3.client('s3')
+
+# 1. List all S3 buckets
+response = s3.list_buckets()
+print("S3 Buckets:")
+for bucket in response['Buckets']:
+    print(f" - {bucket['Name']}")
